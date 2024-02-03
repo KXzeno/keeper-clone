83fa855604c62c019f739d192a4b6b2c09bf96db
git <82054121+KXzeno@users.noreply.github.com>
2024-02-02 19:36:27 -0800

    Use footer element for selector targetting

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,6 +3,8 @@ const year = new Date().getFullYear();
 
 export default function Footer() {
     return(
-        <p>Copyright &copy; {year}</p>
+        <footer>
+            <p>Copyright &copy; {year}</p>
+        </footer>
     );
 }
