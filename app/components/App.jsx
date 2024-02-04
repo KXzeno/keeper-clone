33178c5229ad3c692c248056a66359a624e0c609
git <82054121+KXzeno@users.noreply.github.com>
2024-02-04 10:49:56 -0800

    Add RFC and hooks for destructurized components

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,21 +1,48 @@
 "use client";
+import { useState } from 'react';
 import Footer from './Footer';
 import Note from './Note';
 import '../globals.css';
 import Notes from '../notes.js';
+import CreateArea from './CreateArea';
+
 
 export default function App() {
+    let [items, setItems] = useState([]);
+
+    // To catch nid in lower scopes, cannot simply declare
+    let [nid, setNid] = useState(Number(0));
+
+    let addItem = (item) => {
+        setNid(++nid); // Postfix returns original value before increment
+        item.nid = nid;
+        setItems(old => [...old, item]);
+    }
+
+    let delNote = (event) => {
+        let sid = event.target.id;
+        setItems(old => old.filter(({nid}) => nid != sid));
+        console.log(sid, nid);
+    }
+
+
     return (
         <div>
-            {Notes.map(p => 
-            <Note 
-                key={p.key}
-                title={p.title}
-                content={p.content}
+            <CreateArea 
+                onAdd={addItem}
             />
-            )}
+            {items.map((item, index) => { 
+                return(
+                    <Note
+                        key={index}
+                        id={item.nid}
+                        title={item.title}
+                        content={item.content}
+                        onClicked={delNote}
+                    />
+                )
+            })}
             <Footer />
-            <script src=".\index.js" type="text/jsx"></script>
         </div>
     );
 }
