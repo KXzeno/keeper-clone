94421ab7d2b9a19d5eed580bd1322b55ec0ea6cd
git <82054121+KXzeno@users.noreply.github.com>
2024-02-03 17:34:07 -0800

    Use map function to dynamically render notes arr

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,11 +2,18 @@
 import Footer from './Footer';
 import Note from './Note';
 import '../globals.css';
+import Notes from '../notes.js';
 
 export default function App() {
     return (
         <div>
-            <Note />
+            {Notes.map(p => 
+            <Note 
+                key={p.key}
+                title={p.title}
+                content={p.content}
+            />
+            )}
             <Footer />
             <script src=".\index.js" type="text/jsx"></script>
         </div>
