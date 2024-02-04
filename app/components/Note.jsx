7dd95e55bf2524e943fb2bd3f72465981ddc379e
git <82054121+KXzeno@users.noreply.github.com>
2024-02-04 10:51:34 -0800

    Create mappable data for note component

--- app/components/Note.jsx
+++ app/components/Note.jsx
@@ -1,9 +1,12 @@
 "use client";
+import { useState } from 'react';
+
 export default function Note(Note) {
     return(
         <div className="note">
             <h1>{Note.title}</h1>
             <p>{Note.content}</p>
+            <button id={Note.id} onClick={Note.onClicked}>DELETE</button>
         </div>
     );
 }
